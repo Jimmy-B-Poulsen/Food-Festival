7d262a84f85970b048d7349b4fdb75c96c7c9729
Jimmy Beblein Poulsen <jimm6288@edu.campusvejle.dk>
2020-03-25 11:18:28 +0100

    minor fixes

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ if ('loading' in HTMLImageElement.prototype) {
       img.src = img.dataset.src;
     });
   } else {
-    // Dynamically import the LazySizes library
     const script = document.createElement('script');
-    script.src =
-      'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
     document.body.appendChild(script);
   }
